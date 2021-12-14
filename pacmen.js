8ee4bebe93e0c4fd88302805302880feffe07c53
Joanne Dehnbostel <joanne.dehnbostel@gmail.com>
2021-12-14 09:50:46 -0600

    Update pacmen.js

--- pacmen.js
+++ pacmen.js
@@ -1,7 +1,7 @@
 let pos = 0;
 const pacArray = [
-  ['.blob/main/PacMan1.png', 'blob/main/PacMan2.png'],
-  ['.blob/main/PacMan3.png', 'blob/main/PacMan4.png'],
+  ['.images/PacMan1.png', 'images/PacMan2.png'],
+  ['.images/PacMan3.png', 'images/PacMan4.png'],
 ];
 let direction = 0;
 const pacMen = []; // This array holds all the pacmen
